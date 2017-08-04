6cc4bea45e46576efdbc7057f23ca9d85f44f9ab
rodrigoadfaria <rodrigoadfaria@gmail.com>
2017-08-04 16:49:35 -0300

    Authentication module with views/controllers added
    Signed-off-by: rodrigoadfaria <rodrigoadfaria@gmail.com>

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,29 @@
-angular
-  .module('thinkster', []);
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster', [
+      'thinkster.config',
+      'thinkster.routes',
+      'thinkster.authentication',
+      'thinkster.layout'
+    ])
+    .run(run);
+
+  angular
+    .module('thinkster.routes', ['ngRoute']);
+
+  angular
+    .module('thinkster.config', []);
+
+  run.$inject = ['$http'];
+
+  /**
+  * @name run
+  * @desc Update xsrf $http headers to align with Django's defaults
+  */
+  function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  }
+})();
